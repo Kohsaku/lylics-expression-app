5047ded81b8cbf536b759a954bf6b248a900d02c
Kohsaku <kosakuhayashi@gmail.com>
2022-05-20 23:50:02 +0900

    edit currentfeed component

--- src/components/CurrentFeed.tsx
+++ src/components/CurrentFeed.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography, Paper, ButtonBase } from "@mui/material";
 
 const CurrentFeed: React.FC = () => {
   const [testData, setTestData] = useState([
@@ -19,29 +19,43 @@ const CurrentFeed: React.FC = () => {
     },
   ]);
   return (
-    <Box
-      sx={{
-        my: 8,
-        mx: 4,
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
+    <Box sx={{ my: 8 }}>
       <Typography variant="h5">最近のフィード</Typography>
-      <Grid
-        container
-        flexDirection="column"
-        sx={{ mt: 3, alignItems: "center" }}
+      <Box
+        sx={{
+          mx: 4,
+          display: "flex",
+        }}
       >
-        {testData.map((data) => (
-          <Grid item>
-            <Box p={4} sx={{ border: "1px solid grey" }}>
-              <Typography variant="h6">{data.song}</Typography>
-              <Typography variant="h6">{data.artist}</Typography>
-            </Box>
-          </Grid>
-        ))}
-      </Grid>
+        <Grid container xs={12} sx={{ mt: 3 }}>
+          <Paper sx={{ width: "100%" }}>
+            {testData.map((data) => (
+              <Grid item>
+                <ButtonBase sx={{ width: "100%" }}>
+                  <Box
+                    p={4}
+                    sx={{
+                      border: "1px solid gray",
+                      width: "100%",
+                      textAlign: "left",
+                    }}
+                  >
+                    <Typography variant="h6">曲名：{data.song}</Typography>
+                    <Typography variant="h6">
+                      アーティスト：{data.artist}
+                    </Typography>
+                    <Typography variant="subtitle2">
+                      翻訳：{data.translater}
+                    </Typography>
+                    <Typography variant="subtitle2">英詞：{data.wm}</Typography>
+                    <Typography variant="subtitle2">和訳：{data.jm}</Typography>
+                  </Box>
+                </ButtonBase>
+              </Grid>
+            ))}
+          </Paper>
+        </Grid>
+      </Box>
     </Box>
   );
 };
